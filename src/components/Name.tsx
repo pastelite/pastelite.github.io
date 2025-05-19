fec4944d1c2136b619b37992aa53c5cf7e301dff
pastelite <kana.bhong@gmail.com>
2025-05-19 23:14:18 +0700

    fixing jiterring, assume the width of svg so it's consistent

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -8,61 +8,34 @@ import { animate } from "motion";
 import { useElementSizeCSSVars } from "../hooks/useElementCSSVariable";
 
 // how "p" in the title takes up entire title space
-const pRatio = 0.12;
+const pRatio = 0.115;
 const menuBarWidth = 100;
 const topLeftNameRatio = 0.3; // how much smaller the title is in the top left corner
-const nameBackgroundWidth = 70
-
+const nameBackgroundWidth = 70;
+const animateDelay = 500;
 
 export default function Name() {
-  const [top, setTop] = useState(0);
-  const [screenWidth, setScreenWidth] = useState(0);
-  const [svgWidth, setSvgWidth] = useState(0);
-  // const [scale, setScale] = useState(1);
-  // const calculatedTop = useRef(0); 
-  // const top = useMotionValue(0);
+  const [top, setTop] = useState(240);
+  const [screenWidth, setScreenWidth] = useState(1280);
+  const [svgWidthHeightRatio, setSvgWidthHeightRatio] = useState(0);
   const scale = useMotionValue(1);
-  // const yRaw = useMotionValue(0);
-  // const y = useTransform([yRaw, top], ([vyRaw, vtop]) => `calc(${vtop}px + ${vyRaw}%)`);
-  // const xRaw = useMotionValue(-50);
-  // const screenWidth = useMotionValue(window.innerWidth);
-  // const x = useTransform([screenWidth,xRaw], ([vwidth, v]) => `calc(${vwidth}px + ${v}%)`);
   const [hiding, setHiding] = useState(false);
-  // const height = useMotionValue(160);
+  const [readyToAnimate, setReadyToAnimate] = useState(false);
 
-  let elementRef = useElementSizeCSSVars<HTMLDivElement>("name");
-  // let svgRef = useRef<SVGSVGElement>(null);
+  let svgRef = useRef<SVGSVGElement>(null);
 
   useLayoutEffect(() => {
-    if (elementRef.current) {
-      // setSvgWidth(elementRef.current.getBoundingClientRect().width);
-      setSvgWidth(elementRef.current.offsetWidth);
+    if (svgRef.current) {
+      let svgWidthHeightRatio = svgRef.current.width.animVal.value / svgRef.current.height.animVal.value
+      setSvgWidthHeightRatio(svgWidthHeightRatio);
     }
-    console.log(svgWidth);
+
+    // delay animation
+    setTimeout(() => {
+      setReadyToAnimate(true);
+    }, animateDelay);
   }, []);
 
-  useThrottleScroll(() => {
-    // hiding title
-    let limit = window.innerHeight * 0.5;
-
-    if (window.scrollY > limit) {
-      animate(scale, 1, { duration: 0.1, ease: "easeOut" });
-      setHiding(true);
-      return
-    } else {
-      if (hiding) setHiding(false);
-    }
-
-    // calculate scale
-    let newScale = mappingNumber(
-      1 - window.scrollY / window.innerHeight,
-      0.8,
-      1,
-    );
-
-    animate(scale, newScale, { duration: 0.2, ease: "easeOut" });
-  });
-  
   useLayoutEffect(() => {
     function handleResize() {
       let screenHeight = window.innerHeight;
@@ -77,51 +50,82 @@ export default function Name() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  let pSize = svgWidth * pRatio;
+  useThrottleScroll(() => {
+    // hiding title
+    let limit = window.innerHeight * 0.5;
+
+    if (window.scrollY > limit) {
+      animate(scale, 1, { duration: 0.1, ease: "easeOut" });
+      setHiding(true);
+      return;
+    } else {
+      if (hiding) setHiding(false);
+    }
+
+    // calculate scale
+    let newScale = mappingNumber(
+      1 - window.scrollY / window.innerHeight,
+      0.8,
+      1,
+    );
+
+    animate(scale, newScale, { duration: 0.2, ease: "easeOut" });
+  });
+
+  let presumedSvgWidth = 160 * svgWidthHeightRatio;
+  let pSize = presumedSvgWidth * pRatio;
   let leftOffset = menuBarWidth - pSize + 16; // 16 is margin
 
   return (
     <>
       <motion.div
-        className={"title-name"+(hiding ? " to-top" : "")}
+        className={"title-name" + (hiding ? " to-top" : "")}
         animate={{
-          height: hiding ? topLeftNameRatio * 180 : 180,
+          height: hiding ? topLeftNameRatio * 160 : 160,
           top: hiding ? 0 : top,
           // use calc to reduce the "left" animation
           // /8 since the height is reduce from 180 to 60 (/2)
           // x: hiding ? `calc(0% + ${((menuBarWidth-(svgWidth*pRatio))/2) + svgWidth*pRatio/8}px)` : `calc(-50% + ${screenWidth / 2}px)`,
-          x: hiding? `calc(0% + ${leftOffset}px)` : `calc(-50% + ${screenWidth / 2}px)`,
+          x: hiding
+            ? `calc(0% + ${leftOffset}px)`
+            : `calc(-50% + ${screenWidth / 2}px)`,
           // x: hiding? (menuBarWidth-(svgWidth*pRatio))/2 : -0.5*svgWidth + screenWidth/2,
-          margin: hiding ? "20px 16px": 0,
+          margin: hiding ? "20px 16px" : 0,
           // paddingLeft: `calc(${pRatio} * (var(--name-width) - 2em))`,
-          
         }}
         style={{
           scale: scale,
         }}
-        transition={{ duration: 0.2, ease: "easeOut" }}
-        ref={elementRef}
+        transition={{ duration: readyToAnimate ? 0.2 : 0, ease: "easeOut" }}
       >
         {/* The second div for the background. This is easier to be it's own thing but it's kinda need to inherit the position of the main text as well */}
-        <div style={{
-          height: hiding? nameBackgroundWidth : "100%",
-          width: hiding? nameBackgroundWidth: "100%",
-          position: "absolute",
-          background: "gray",
-          borderRadius: "25%",
-          opacity: hiding? 1: 0,
-          left: (menuBarWidth-nameBackgroundWidth)/2 - leftOffset - 16,
-          top: "calc(50% + 10%)", // accounted for the weird p shape
-          transform: "translateY(-50%)",
-          transition: "all .2s ease-out"
-        }}>
+        <motion.div
+          className="title-name-background"
+          animate={{
+            height: hiding ? nameBackgroundWidth : "100%",
+            width: hiding ? nameBackgroundWidth : "100%",
+            position: "absolute",
+            background: "gray",
+            borderRadius: hiding ? nameBackgroundWidth * 0.25 : 180 * 0.25,
+            opacity: hiding ? 1 : 0,
+            left: (menuBarWidth - nameBackgroundWidth) / 2 - leftOffset - 16,
+            top: "calc(50% + 10%)", // accounted for the weird p shape
+            transform: "translateY(-50%)",
+            scale: hiding ? 1 : 1.2,
+          }}
+          transition={{ duration: readyToAnimate ? 0.2 : 0, ease: "easeOut" }}
+        >
+        </motion.div>
+        <PasteliteSvg
+          style={{
+            clipPath: hiding
+              ? "polygon(0px 0px, 13% 0px, 10% 100%, 0px 100%)"
+              : "polygon(-10px -10px, 110% -10px, 110% 110%, -10px 110%)",
+            transition: `clip-path ${readyToAnimate ? 0.2 : 0}s ease-out`,
+          }}
 
-        </div>
-        <PasteliteSvg style={{
-          clipPath: hiding ? "polygon(0px 0px, 13% 0px, 10% 100%, 0px 100%)" 
-          : "polygon(-10px -10px, 110% -10px, 110% 110%, -10px 110%)",
-          transition: "clip-path .2s ease-out"
-        }} />
+          ref={svgRef}
+        />
       </motion.div>
     </>
   );
